463e982521552cbd98204dad04f1606dbf26803e
Jacob Rocque <jacobrocque@gmail.com>
2025-02-17 18:41:22 -0500

    initialize

---
+++ src/main/java/com/jacobrocque/myancestrytree/MyAncestryTreeApplication.java
@@ -0,0 +1,13 @@
+package com.jacobrocque.myancestrytree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAncestryTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAncestryTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacobrocque/myancestrytree/MyAncestryTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacobrocque.myancestrytree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAncestryTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
