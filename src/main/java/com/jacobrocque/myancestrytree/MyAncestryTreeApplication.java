20b7e19247a35bb6ed91da66a01bcb4cbd53add7
Jacob Rocque <jacobrocque@gmail.com>
2025-02-17 18:41:44 -0500

    initialize

--- src/main/java/com/jacobrocque/myancestrytree/MyAncestryTreeApplication.java
+++ src/main/java/com/jacobrocque/myancestrytree/MyAncestryTreeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyAncestryTreeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyAncestryTreeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyAncestryTreeApplication.class, args);
+    }
 
 }
